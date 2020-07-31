4a7481026057ca0c369bcf5802a42ddf14e45745
Chamil <chamiljay88@gmail.com>
2020-07-31 12:07:21 +0530

    2020-07-31 morning

--- src/tab/PeriodCalandar.js
+++ src/tab/PeriodCalandar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, StyleSheet, Text, View, SafeAreaView, TouchableOpacity,ScrollView } from 'react-native';
+import { Modal, StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
 
 import { CustomHeader } from '../index';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
@@ -51,40 +51,48 @@ export class PeriodCalandar extends Component {
     componentDidMount() {
         db.loadDB();
 
-
-
+        let selected = true;
+        let markedDates = {}
 
         let products = [];
         db.listProduct().then((data) => {
             products = data;
-            let selected = true;
-            let markedDates = {}
             for (var i = 0; i < products.length; i++) {
                 _pdate = products[i].pName
                 if (this.state._markedDates[_pdate]) {
                     selected = !this.state._markedDates[_pdate].selected;
                     markedDates = this.state._markedDates[_pdate];
-                    // console.log("lllllllllllllllllllllllllllllllll..."+_pdate);
+                }
+                if (_pdate.substring(0, 7) == _today.substring(0, 7)) {
+
                 }
 
                 markedDates = { ...markedDates, ...{ selected }, selectedColor: "red" };
                 updatedMarkedDates = { ...this.state._markedDates, ...{ [_pdate]: markedDates } }
-                //  console.log("lllllllllllllllllllllllllllllllll ?>>>.s.."+updatedMarkedDates);
                 this.setState({
                     products,
                     isLoading: false,
+
                     _markedDates: updatedMarkedDates,
                     pName: _pdate
                 });
             }
-
-
         }).catch((err) => {
             console.log(err);
             this.setState = {
                 isLoading: false
             }
         })
+        const difdate = "2020-08-22";
+
+        markedDates = { ...markedDates, ...{ selected }, selectedColor: "green" };
+        updatedMarkedDates = { ...this.state._markedDates, ...{ [difdate]: markedDates } }
+      
+        this.setState({
+            _markedDates: updatedMarkedDates,
+        });
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>> ?>>>.s : " + difdate);
+
     }
     savePeriod() {
         this.setState({
@@ -152,116 +160,117 @@ export class PeriodCalandar extends Component {
         console.log("set sate eke value eka  : " + this.state.pName);
         return (
             <SafeAreaView style={{ flex: 1, backgroundColor: '#fce4ec' }}>
-                  <ScrollView
+                <ScrollView
                     contentInsetAdjustmentBehavior="automatic"
                     style={styles.scrollView}>
-                <CustomHeader bgcolor='white' title="Home detail" navigation={this.props.navigation} bdcolor='#f2f2f2' />
-                <View style={{ flex: 1, padding:10}}>
+                    <CustomHeader bgcolor='white' title="Home detail" navigation={this.props.navigation} bdcolor='#f2f2f2' />
+                    <View style={{ flex: 1, padding: 10 }}>
 
-                    <Card>
-                        {/* <Card.Title title="Card Title" subtitle="Card Subtitle" /> */}
-                        <Card.Content>
-                         
-                            {/* <Paragraph>Card content</Paragraph> */}
-                            <Calendar
-                        theme={{
-                            dotColor: 'pink',
-                            // backgroundColor: 'white',
-                            // selectedDayBackgroundColor: 'white',
-                            selectedDayTextColor: 'white'
-                        }}
+                        <Card>
+                            {/* <Card.Title title="Card Title" subtitle="Card Subtitle" /> */}
+                            <Card.Content>
 
-                        // we use moment.js to give the minimum and maximum dates.
-                        minDate={_today}
-                        // maxDate={_maxDate}
+                                {/* <Paragraph>Card content</Paragraph> */}
+                                <Calendar
+                                    theme={{
+                                        dotColor: 'pink',
+                                        // backgroundColor: 'white',
+                                        // selectedDayBackgroundColor: 'white',
+                                        selectedDayTextColor: 'white'
+                                    }}
 
-                        // hideArrows={true}
-
-                        // onDayPress={this.onDaySelect}
-                        // onPress={() => this.RBSheet.open()}
-                        onDayPress={this.onDaySelect}
-
-                        // onDaySelect={()=>this.RBSheet.open()}
-
-                        // markedDates={{
-                        //     '2020-07-25': { selected: true, selectedColor: 'green' },
-                        //     '2020-07-26': { selected: true, selectedColor: 'red' }
-                        // }}
-
-                        // markingType='multi-period'
-                        markedDates={this.state._markedDates}
-                    // markingType={'multi-dot'}
-
-                    />
-                        </Card.Content>
-
-                    </Card>
-                  
-                    <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
-                        <Text style={{ color: 'grey' }}>Period</Text>
-                        <Text style={{ fontSize: 40, }}>3 Days left</Text>
-                        <Text style={{ color: 'grey' }}>Ovulation 12 days left</Text>
+                                    // we use moment.js to give the minimum and maximum dates.
+                                    minDate={_today}
+                                    // maxDate={_maxDate}
 
 
-                        <TouchableOpacity style={{ marginTop: 30 }} onPress={() => this.props.navigation.navigate('TestScreeen')} >
-                            <Text>dsdsdsd</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{ marginTop: 30 }} onPress={() => this.props.navigation.navigate('ProductScreen2')} >
-                            <Text>View</Text>
-                        </TouchableOpacity>
-                    </View>
+                                    // hideArrows={true}
+
+                                    // onDayPress={this.onDaySelect}
+                                    // onPress={() => this.RBSheet.open()}
+                                    onDayPress={this.onDaySelect}
+
+                                    // onDaySelect={()=>this.RBSheet.open()}
+
+                                    // markedDates={{
+                                    //     '2020-08-25': { selected: true, selectedColor: 'green' },
+                                    //     '2020-08-26': { selected: true, selectedColor: 'red' }
+                                    // }}
+
+                                    // markingType='multi-period'
+                                    markedDates={this.state._markedDates}
+                                // markingType={'multi-dot'}
+
+                                />
+                            </Card.Content>
+
+                        </Card>
+
+                        <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+                            <Text style={{ color: 'grey' }}>Period</Text>
+                            <Text style={{ fontSize: 40, }}>3 Days left</Text>
+                            <Text style={{ color: 'grey' }}>Ovulation 12 days left</Text>
 
 
-                    <RBSheet
-                        ref={ref => {
-                            this.RBSheet = ref;
-                        }}
-                        height={300}
-                        openDuration={250}
-                        customStyles={{
-                            container: {
-                                justifyContent: "center",
-                                alignItems: "center",
-                                borderTopRightRadius: 20,
-                                borderTopLeftRadius: 20
-                            }
-                        }}
-
-                    >
-                        <View style={{ flex: 1 }}>
-
-                            <Text >  </Text>
-                            {/* onPress={() => alert('Hello, world!')} */}
-                            <TouchableOpacity onPress={() => this.savePeriod()} style={styles.button}>
-                                <Text style={styles.buttonText}>Pick a photo</Text>
-
-
+                            <TouchableOpacity style={{ marginTop: 30 }} onPress={() => this.props.navigation.navigate('TestScreeen')} >
+                                <Text>dsdsdsd</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity onPress={() => this.geteriods()} style={styles.button}>
-                                <Text style={styles.buttonText}>view period</Text>
-
-
+                            <TouchableOpacity style={{ marginTop: 30 }} onPress={() => this.props.navigation.navigate('ProductScreen2')} >
+                                <Text>View</Text>
                             </TouchableOpacity>
+                        </View>
 
 
-                            {/* <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center', marginVertical: 24 }}>
+                        <RBSheet
+                            ref={ref => {
+                                this.RBSheet = ref;
+                            }}
+                            height={300}
+                            openDuration={250}
+                            customStyles={{
+                                container: {
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderTopRightRadius: 20,
+                                    borderTopLeftRadius: 20
+                                }
+                            }}
+
+                        >
+                            <View style={{ flex: 1 }}>
+
+                                <Text >  </Text>
+                                {/* onPress={() => alert('Hello, world!')} */}
+                                <TouchableOpacity onPress={() => this.savePeriod()} style={styles.button}>
+                                    <Text style={styles.buttonText}>Pick a photo</Text>
+
+
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => this.geteriods()} style={styles.button}>
+                                    <Text style={styles.buttonText}>view period</Text>
+
+
+                                </TouchableOpacity>
+
+
+                                {/* <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center', marginVertical: 24 }}>
 
 
                             </View> */}
 
-                            <TextInput value={this.state.pId} onChangeText={(text) => this.updateTextInput(text, 'pId')} style={{ height: 50, width: 250 }} />
+                                <TextInput value={this.state.pId} onChangeText={(text) => this.updateTextInput(text, 'pId')} style={{ height: 50, width: 250 }} />
 
-                            <TextInput value={this.state.pName} onChangeText={(text) => this.updateTextInput(text, 'pName')} style={{ height: 50, width: 250 }} />
-
-
-                        </View>
-                    </RBSheet>
-                </View>
-
-                <View style={{ flex: 1 }}>
+                                <TextInput value={this.state.pName} onChangeText={(text) => this.updateTextInput(text, 'pName')} style={{ height: 50, width: 250 }} />
 
 
-                </View>
+                            </View>
+                        </RBSheet>
+                    </View>
+
+                    <View style={{ flex: 1 }}>
+
+
+                    </View>
                 </ScrollView>
             </SafeAreaView>
         );
