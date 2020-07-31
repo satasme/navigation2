e60de9dfc1e402e839ad2eaaf02c33b36d2ec096
Chamil <chamiljay88@gmail.com>
2020-07-31 08:56:30 +0530

    2020-07-31 morning push

--- src/tab/PeriodCalandar.js
+++ src/tab/PeriodCalandar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Modal, StyleSheet, Text, View, SafeAreaView, TouchableOpacity,ScrollView } from 'react-native';
 
 import { CustomHeader } from '../index';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
@@ -7,7 +7,7 @@ import moment from 'moment' // 2.20.1
 
 // import SwipeablePanel from 'rn-swipeable-panel';
 import RBSheet from "react-native-raw-bottom-sheet";
-import { TextInput } from 'react-native-paper';
+import { TextInput, Card, Title, Paragraph } from 'react-native-paper';
 import { Button } from 'react-native-elements';
 
 import Database from '../Database';
@@ -18,9 +18,12 @@ const _format = 'YYYY-MM-DD'
 const _today = moment().format(_format)
 const _maxDate = moment().add(31, 'days').format(_format)
 
-const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
-const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
-const workout = { key: 'workout', color: 'green' };
+// '2012-05-16': {selected: true, marked: true, selectedColor: 'blue'},
+// const vacation = { key: 'vacation', color: 'red', selectedColor: 'blue' };
+// const massage = { key: 'massage', color: 'blue', selectedColor: 'blue' };
+// const workout = { key: 'workout', color: 'green' };
+
+
 
 
 const db = new Database();
@@ -47,6 +50,41 @@ export class PeriodCalandar extends Component {
     }
     componentDidMount() {
         db.loadDB();
+
+
+
+
+        let products = [];
+        db.listProduct().then((data) => {
+            products = data;
+            let selected = true;
+            let markedDates = {}
+            for (var i = 0; i < products.length; i++) {
+                _pdate = products[i].pName
+                if (this.state._markedDates[_pdate]) {
+                    selected = !this.state._markedDates[_pdate].selected;
+                    markedDates = this.state._markedDates[_pdate];
+                    // console.log("lllllllllllllllllllllllllllllllll..."+_pdate);
+                }
+
+                markedDates = { ...markedDates, ...{ selected }, selectedColor: "red" };
+                updatedMarkedDates = { ...this.state._markedDates, ...{ [_pdate]: markedDates } }
+                //  console.log("lllllllllllllllllllllllllllllllll ?>>>.s.."+updatedMarkedDates);
+                this.setState({
+                    products,
+                    isLoading: false,
+                    _markedDates: updatedMarkedDates,
+                    pName: _pdate
+                });
+            }
+
+
+        }).catch((err) => {
+            console.log(err);
+            this.setState = {
+                isLoading: false
+            }
+        })
     }
     savePeriod() {
         this.setState({
@@ -76,64 +114,61 @@ export class PeriodCalandar extends Component {
         this.setState(state);
     }
     geteriods() {
-        // console.log("lllllllllllllllllllllllllllllllll...");
-        let products = [];
-        db.listProduct().then((data) => {
-          products = data;
-          console.log("period tttttrrrrrrrrrrrrrrrrrrrrrrrrrrrr :  "+data);
-          this.setState({
-            products,
-            isLoading: false,
-          });
-        }).catch((err) => {
-          console.log(err);
-          this.setState = {
-            isLoading: false
-          }
-        })
-      }
+
+    }
 
     onDaySelect = (day) => {
         this.RBSheet.open();
         const _selectedDay = moment(day.dateString).format(_format);
-        console.log("ssssssssssssssssss : " + _selectedDay);
 
-        let marked = true;
-        let markedDates = {}
+
+        // let marked = true;
+        // let markedDates = {}
         if (this.state._markedDates[_selectedDay]) {
             // Already in marked dates, so reverse current marked state
-            marked = !this.state._markedDates[_selectedDay].marked;
-            markedDates = this.state._markedDates[_selectedDay];
+            // marked = !this.state._markedDates[_selectedDay].marked;
+            // markedDates = this.state._markedDates[_selectedDay];
         }
 
-        markedDates = { ...markedDates, ...{ marked } };
+        // markedDates = { ...markedDates, ...{ marked } };
 
         // Create a new object using object property spread since it should be immutable
         // Reading: https://davidwalsh.name/merge-objects
-        const updatedMarkedDates = { ...this.state._markedDates, ...{ [_selectedDay]: markedDates } }
+        //const updatedMarkedDates = { ...this.state._markedDates, ...{ [_selectedDay]: markedDates } }
 
         // Triggers component to render again, picking up the new state
         this.setState({
-            _markedDates: updatedMarkedDates,
+            // _markedDates: updatedMarkedDates,
             pName: _selectedDay
         });
 
     }
 
 
-    renderContent = () => { }
+    // renderContent = () => { }
 
     renderHeader = () => { }
     render() {
         console.log("set sate eke value eka  : " + this.state.pName);
         return (
             <SafeAreaView style={{ flex: 1, backgroundColor: '#fce4ec' }}>
+                  <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={styles.scrollView}>
                 <CustomHeader bgcolor='white' title="Home detail" navigation={this.props.navigation} bdcolor='#f2f2f2' />
-                <View style={{ flex: 1, }}>
-                    <Calendar
+                <View style={{ flex: 1, padding:10}}>
+
+                    <Card>
+                        {/* <Card.Title title="Card Title" subtitle="Card Subtitle" /> */}
+                        <Card.Content>
+                         
+                            {/* <Paragraph>Card content</Paragraph> */}
+                            <Calendar
                         theme={{
                             dotColor: 'pink',
-                            backgroundColor: '#ccc'
+                            // backgroundColor: 'white',
+                            // selectedDayBackgroundColor: 'white',
+                            selectedDayTextColor: 'white'
                         }}
 
                         // we use moment.js to give the minimum and maximum dates.
@@ -147,8 +182,21 @@ export class PeriodCalandar extends Component {
                         onDayPress={this.onDaySelect}
 
                         // onDaySelect={()=>this.RBSheet.open()}
+
+                        // markedDates={{
+                        //     '2020-07-25': { selected: true, selectedColor: 'green' },
+                        //     '2020-07-26': { selected: true, selectedColor: 'red' }
+                        // }}
+
+                        // markingType='multi-period'
                         markedDates={this.state._markedDates}
+                    // markingType={'multi-dot'}
+
                     />
+                        </Card.Content>
+
+                    </Card>
+                  
                     <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
                         <Text style={{ color: 'grey' }}>Period</Text>
                         <Text style={{ fontSize: 40, }}>3 Days left</Text>
@@ -184,17 +232,17 @@ export class PeriodCalandar extends Component {
 
                             <Text >  </Text>
                             {/* onPress={() => alert('Hello, world!')} */}
-                            <TouchableOpacity     onPress={() => this.savePeriod()}  style={styles.button}>
+                            <TouchableOpacity onPress={() => this.savePeriod()} style={styles.button}>
                                 <Text style={styles.buttonText}>Pick a photo</Text>
 
 
                             </TouchableOpacity>
-                            <TouchableOpacity     onPress={() => this.geteriods()}  style={styles.button}>
-                                <Text style={styles.buttonText}>Pick a photo</Text>
+                            <TouchableOpacity onPress={() => this.geteriods()} style={styles.button}>
+                                <Text style={styles.buttonText}>view period</Text>
 
 
                             </TouchableOpacity>
-                            
+
 
                             {/* <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center', marginVertical: 24 }}>
 
@@ -214,7 +262,7 @@ export class PeriodCalandar extends Component {
 
 
                 </View>
-
+                </ScrollView>
             </SafeAreaView>
         );
     }
